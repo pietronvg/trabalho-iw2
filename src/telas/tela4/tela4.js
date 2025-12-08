d3b141365c6c54c49e3c30c9b34d09d7199b1c7d
Pietro <seu@email.com>
2025-12-07 22:11:02 -0300

    Atualizadas telas e configurado React Router

--- src/telas/tela4/tela4.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ backgroundColor: "red", height: "100vh", padding: 20 }}>
-      <h1>Página 4</h1>
-      <p>Bem-vindo à página!</p>
-    </div>
-  );
-}
